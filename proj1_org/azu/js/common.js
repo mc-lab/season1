d2d1c9ad3a539278ea56db9ea7d49ec83420c22e
azuma <azuma@weddingpark.co.jp>
2015-11-04 15:09:20 +0900

    不要ファイルの削除

--- proj1_org/azu/js/common.js
+++ proj1_org/azu/js/common.js
@@ -1,19 +1,20 @@
 $(function(){
-  $('.dynamic-list .animation-item').css('visibility','hidden');
+  $('.dynamic-list ka_animation-item').css('visibility','hidden');
   $(window).scroll(function(){
-      $('.dynamic-list .animation-item:odd').each(function(){
+      $('.dynamic-list .ka_animation-item:odd').each(function(){
         var pos = $(this).offset().top;
         if ($(window).scrollTop() + $(window).height() > pos) {
             $(this).css('visibility','visible');
             $(this).addClass('animeL');
         }
       });
-      $('.dynamic-list .animation-item:even').each(function(){
+      $('.dynamic-list .ka_animation-item:even').each(function(){
         var pos = $(this).offset().top;
         if ($(window).scrollTop() + $(window).height() > pos) {
             $(this).css('visibility','visible');
             $(this).addClass('animeR');
         }
-      });  });
+      });  
+  }).scroll();
 });
 
