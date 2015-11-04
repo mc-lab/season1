728b11d5d7c1a802e80832248a44450fd9505f9d
sasano <sasano@weddingpark.co.jp>
2015-11-04 20:15:57 +0900

    finalの作成＆少しマージ

---
+++ proj1_org/final/js/common.js
@@ -0,0 +1,16 @@
+$(function() {
+    setInterval(function(){
+        $('.wrapper').show();
+    } , '5000');
+
+
+    $.ajax({
+        url: 'contents.html',
+        type: 'GET',
+        dataType: 'html'
+    })
+    .success(function(res) {
+        $('.wrapper').append(res);
+
+    });
+});
