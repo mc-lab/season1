f14ff1af4e7b9588ca3ffc87827773466da547b7
azuma <azuma@weddingpark.co.jp>
2015-11-10 11:22:10 +0900

    レトロRPG風カセット

--- proj1_org/azu/js/common.js
+++ proj1_org/azu/js/common.js
@@ -1,30 +1,29 @@
 $(function(){
+  $('.dynamic-list .ka_animation-item').css('visibility','hidden');
 
-  $('.dynamic-list ka_animation-item').css('visibility','hidden');
   $(window).scroll(function(){
       $('.dynamic-list .ka_animation-item:odd').each(function(){
         var pos = $(this).offset().top;
         if ($(window).scrollTop() + $(window).height() > pos) {
             $(this).css('visibility','visible');
-            $(this).addClass('animeL');
-            // $('.ka_movie',this).addClass('ka_animate');
+            $(this).addClass('ka_animeL');
         }
       });
       $('.dynamic-list .ka_animation-item:even').each(function(){
         var pos = $(this).offset().top;
         if ($(window).scrollTop() + $(window).height() > pos) {
             $(this).css('visibility','visible');
-            $(this).addClass('animeR');
-            // $('.ka_movie',this).addClass('ka_animate');
+            $(this).addClass('ka_animeR');
         }
       });
   }).scroll();
-  $('.ka_movie').click(function(){
+
+  $('.ka_play').click(function(){
     if($(this).hasClass('ka_clicked') == false) {
       $(this).addClass('ka_clicked');
       $(this).addClass('ka_animate');
       var self = $(this);
-      setInterval(function(){play(self)}, 2000);
+      // setInterval(function(){play(self)}, 100);
     }
   });
 
