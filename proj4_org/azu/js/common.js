5e5671ab2c6752fb6502c2da650e1fc06f59d786
azuma <G00059@WP3N-152.cyberagent.co.jp>
2016-06-29 13:43:35 +0900

    マージ完了

--- proj4_org/azu/js/common.js
+++ proj4_org/azu/js/common.js
@@ -1,7 +1,27 @@
 (function(){
-	$(function(){
+
+	setTimeout(function(){
+	    $('.hn_loading').fadeOut();
+	    $('.hn_main-visual').show();
+	    $('.hn_container').addClass('active');
+	} , '5000');
+
+	setTimeout(function(){
+	    $('.hn_main-visual').remove();
+	    $('.hn_loading').remove();
+	    $('html').removeClass('now_loading');
+	    $('.hn-cover').remove();
+	} , '9000');
+
+	setTimeout(function(){
+	    $('.wrapper').show();
+	    completeToRead();
+	} , '9500');
+
+	function completeToRead(){
 		mt = 0;
 		FRAME = 360;
+		renderFlag = true;
 
 		MAX_MARGIN_TOP = -parseInt($('.img').css('height'), 10);
 		$('.obj').css('height', MAX_MARGIN_TOP+'pt');
@@ -12,12 +32,17 @@
 	        if (mt >= 0) {
 	    		$('#scroll-message').show();
 		    	$('.img').css({"margin-top": mt});
+				renderFlag = true;
 	        } else if (mt > MAX_MARGIN_TOP) {
 	    		$('#scroll-message').hide();
 		    	$('.img').css({"margin-top": mt});
 	        	$('.back').css('display', 'none');
 	        } else {
 	        	$('.back').slideDown();
+				if(renderFlag) {
+			    	$('.img').css({"margin-top": mt});
+			    	renderFlag = false;
+				}
 	        }
 	    });
 
@@ -35,5 +60,5 @@
 				{scrollTop: 0}, 30000, 'linear'
 			);
 		});
-	});
+	}
 })();
