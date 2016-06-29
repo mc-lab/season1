f152cc36b6a8b7008e91a45b4cad27b2c7c535e9
azuma <G00059@WP3N-152.cyberagent.co.jp>
2016-06-29 10:24:54 +0900

    コンテンツ完成

--- proj4_org/azu/js/common.js
+++ proj4_org/azu/js/common.js
@@ -4,11 +4,16 @@
 		FRAME = 360;
 
 		MAX_MARGIN_TOP = -parseInt($('.img').css('height'), 10);
-		$('.obj').css('height', (parseInt($('.img').css('height'), 10) + FRAME))+'pt';
-		$('.contents').scrollTop(0,0);
-	    $('.contents').scroll(function(){
-	    	mt = Math.floor($(this).scrollTop() / 360) * -FRAME;
-	        if (mt > MAX_MARGIN_TOP) {
+		$('.obj').css('height', MAX_MARGIN_TOP+'pt');
+		$('.obj-wrap').scrollTop(0,0);
+
+	    $('.obj-wrap').scroll(function(){
+	    	mt = Math.floor($(this).scrollTop() / 180) * -FRAME;
+	        if (mt >= 0) {
+	    		$('#scroll-message').show();
+		    	$('.img').css({"margin-top": mt});
+	        } else if (mt > MAX_MARGIN_TOP) {
+	    		$('#scroll-message').hide();
 		    	$('.img').css({"margin-top": mt});
 	        	$('.back').css('display', 'none');
 	        } else {
@@ -16,9 +21,19 @@
 	        }
 	    });
 
+		$('#scroll-message').click(function(){
+			$('.obj-wrap').animate(
+				{scrollTop: (-MAX_MARGIN_TOP + FRAME) / 2 }, 
+				60000, 'linear'
+			);
+		});
+
+
 		$('.back').click(function(){
-			$('.contents').animate({scrollTop: 0}, {
-			duration: 5000});
+			$('.obj-wrap').scrollTop((-MAX_MARGIN_TOP + FRAME) / 2);
+			$('.obj-wrap').animate(
+				{scrollTop: 0}, 30000, 'linear'
+			);
 		});
 	});
 })();
